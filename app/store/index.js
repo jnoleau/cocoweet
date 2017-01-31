e43a8e578f1172857b6334d2d7298d43917e21c7
Julien Noleau <julien@devanco.com>
2017-01-31 09:20:24 +0100

    delete legacy store

--- app/store/index.js
+++ app/store/index.js
@@ -1,6 +1,5 @@
 // @flow
 import type {State} from 'app/store/state';
-import type {Store as StoreLib} from 'app/store/lib';
 
 import deepmerge from 'deepmerge';
 import {applyMiddleware, compose} from 'redux';
@@ -8,8 +7,6 @@ import createLogger from 'redux-logger';
 import {createStore} from 'redux-simplux';
 import {getInitial} from 'app/store/state';
 import storageMiddleware from 'app/store/middleware.tostorage';
-
-export type Store = StoreLib<State>;
 
 const storage = storageMiddleware({
   key: 'cocoweet',
@@ -26,9 +23,8 @@ const logger = createLogger();
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // eslint-disable-line no-underscore-dangle, max-len
-const store: Store = createStore(
+
+export default createStore(
   savedState ? deepmerge(initialState, savedState) : initialState,
   composeEnhancers(applyMiddleware(logger, storage.middleware))
 );
-
-export default store;
