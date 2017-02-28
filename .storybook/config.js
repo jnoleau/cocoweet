290a5c6bf4b7d3c87b9d1079c7bde969a1e602e3
Julien Noleau <julien@devanco.com>
2017-02-28 23:00:26 +0100

    decorator

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,16 @@
-import { configure } from '@kadira/storybook';
+import React from 'react';
+import {configure, addDecorator} from '@kadira/storybook';
+import 'app/view/root.less';
+import style from 'app/view/main/index.less';
+
+addDecorator((story) => (
+  <div className={style.main}>
+    {story()}
+  </div>
+));
 
 function loadStories() {
-  require('app/view/stories');
+  require('app/view/stories'); // eslint-disable-line
 }
 
 configure(loadStories, module);
--- app/view/stories/index.js
+++ app/view/stories/index.js
@@ -5,10 +5,18 @@ import React from 'react';
 import {storiesOf} from '@kadira/storybook';
 import tweetExtendedBig from 'app/mock/samples/initial/extended_big';
 
+import style from 'app/view/main/stream.less';
 import StreamTweet from 'app/view/main/stream.tweet';
 
 storiesOf('Tweet', module)
-  .add('extended > 140', (): RE => (
-    <StreamTweet tweet={tweetExtendedBig} />
+  .addDecorator((story: any): RE => (
+    <div className="body">
+      <div className="right">
+        <div className={style.stream}>
+          <ul><li>{story()}</li></ul>
+        </div>
+      </div>
+    </div>
   ))
+  .add('extended > 140', (): RE => <StreamTweet tweet={tweetExtendedBig} />)
 ;
